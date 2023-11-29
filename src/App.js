3c59d23ad8fe38b4fbdafd34bb3d3ee2239b5303
MikeBonWebDev <141561085+MikeBonWebDev@users.noreply.github.com>
2023-11-29 22:02:39 +0100

    Adding comments to NasdApp

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Cerca from './Cerca';
 import NomeStock from './NomeStock';
 import Stock from './Stock';
 
+//Definisco il componente con stato tramite notazione classe
 class App extends Component {
 
   constructor(props) {
     super(props);
 
+    //Definisco l'oggetto state, e le proprietà necessarie
     this.state = {
       listaelementi: [],
       listapreferiti: [],
@@ -22,30 +24,33 @@ class App extends Component {
       noResult: false,
       cont: 0,     
     };
-
-    console.log('1g) Creo istanza GENITORE');
   }
 
-  //Start search and save elements from search input to API Call
-  //On Click Submit button in Cerca.js
+  /*Il metodo passato al componente 'Cerca.js',
+  mi restituisce la stringa che passo poi al metodo interno.
+  Tale stringa andrà a inserirsi nell'URL dell'API Call*/
   cercaElementi =  (strcerca) => {   
     this.getElementi(strcerca);
   }
 
-  /*Call API, manipulate data and save in this.state. Generation NomeStock.js component
-  About error: see example file '.json' in public folder like 'fetchStock.json',
-  for catch behavior*/
+  /*Definisco una funzione asincrona,
+  effettuo un primo tentativo di chiamata API,
+  gestisco gli errori, e se tutto è ok,
+  manipolo i dati salvandoli nelle apposite variabili di stato */
   getElementi = async (str) => {            
 
-      //Using a .env file for more security of my API Key
+      //Using a .env file providing more security of my API Key
       const accessKeyAlphaV = process.env.AV_API_KEY;
       const apiUrl = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${str}&interval=1min&outputsize=compact&apikey=${accessKeyAlphaV}&datatype=json`;
       try {
+        //Inizializzo due proprietà dell'oggetto state
         this.setState({inCaricamento: true, showError: false});
+
+        //Effettuo la chiamata all'API, gestisco la risposta come oggetto JSON
         const response = await fetch(apiUrl);
         const r = await response.json();
 
-        //Start setting error behavior
+        //Inizio a definire le possibili casistiche di errore
         if (Object.keys(r).length === 0) {
           throw new Error(`Errore generico -
           verificare la correttezza delle informazioni inserite`)
@@ -71,16 +76,15 @@ class App extends Component {
             throw new Error(`Elemento già in lista`);
           }
         })
-
-        console.log(`Recupero dati ${JSON.stringify(result)}`);
         
+        //Aggiungo i nuovi elementi alla variabile di stato
         this.setState((prevState) => ({          
           listaelementi : [...prevState.listaelementi, result],
           inCaricamento: false,              
-        }), () => {
-          console.log(`Numero elementi trovati: ${this.state.listaelementi.length}`);                  
-        });        
+        }));  
+
       } catch (error) {
+        //Gestisco gli errori, imposto lo stato, e li riporto in console
         console.log(`Fetch failed, ${error}`);
         this.setState({
           inCaricamento: false,
@@ -90,20 +94,23 @@ class App extends Component {
       };
   }
 
-  //How Stock.js being create
+  /*Cosa accade quando effettuo un 'click' sul comp. 'NomeStock.js',
+  e come viene agganciato 'Stock.js'*/
   onAddPreferiti = (ids) => {
     try {
-      console.log(this.state.cont);
+      //Inizializzo due variabili di stato
       this.setState({
         inCaricamentoStock: true,
         showErrorStock: false,
       });
 
+      /*Tramite l'indice verifico quali elementi ci sono,
+      e se non ci sono lancio 'Error'*/
       if (!this.state.listapreferiti[ids]) {
-
         const newStock = this.state.listaelementi[ids];
         const contatore = this.state.cont + 1;
         newStock['id'] = contatore;
+        
         this.setState({
           listapreferiti: [...this.state.listapreferiti, newStock],
           cont: contatore,
@@ -116,6 +123,7 @@ class App extends Component {
       }
       
     } catch (error){
+      //Gestisco gli errori, imposto lo stato, e li riporto in console
       console.log(`Failed to execute: ${error.message}`);
       this.setState({
         inCaricamentoStock: false,
@@ -125,12 +133,14 @@ class App extends Component {
     }
   }
 
-  //How Stock.js being delete
+  //Come elimino i dati quando elimino il componente 'Stock.js'
   elimino = (id) => {
     if(id) {
+      //Se l'id esiste, filtro tutti gli elementi della lista, tranne quello
       const preferiti = this.state.listapreferiti.filter((el) => {
         return el.id !== id;
       })
+      //Imposto lo stato con i nuovi valori
       this.setState({
         listapreferiti: preferiti,
         showErrorStock: false,
@@ -138,21 +148,23 @@ class App extends Component {
     }
   } 
 
-  render() {
-    console.log('2g) GENITORE Render App');
+  render() {    
     
     return(
       <div className='App container-fluid'>
         <header className='App-header'>
-          
+          {/*Inserisco il componente per effettuare la ricerca*/}
           <Cerca onInputSearch={this.cercaElementi}/>
 
+          {/*Imposto la posizione per gli Error Alert*/}
           {this.state.showError &&
             <p className={this.state.classError}>{this.state.msgError}</p>}
 
+          {/*Imposto un'icona di caricamento*/}
           {this.state.inCaricamento && 
             <p className='loadico text-center fas fa-sync-alt fa-2x mt-3'></p>}          
 
+          {/*Imposto il contenitore per i risultati iniziali della lista stock*/}
           <div className='container'>
             <section className='listastock'>
               <div className='row'>
@@ -169,17 +181,21 @@ class App extends Component {
                 </div>
               </div>
             </section>
+
+            {/*Imposto il contenitore per gli Stock al dettaglio*/}
             <section className='listapreferiti'>
               <div className='row'>
               {this.state.listapreferiti.map((el, index) => 
                 <div className='col-sm-6'>
+
+                    {/*Imposto un'icona di caricamento*/}
                     {this.state.inCaricamentoStock && 
                       <p className='loadico text-center fas fa-sync-alt fa-3x mt-3'></p>}
-
+                    
+                    {/*Imposto gli Error Alert*/}
                     {this.state.showErrorStock && 
                       <p className={this.state.classError}>{this.state.msgErrorStock}</p>}
 
-                    
                       <Stock
                         key={el.id}
                         datistock= {el}
@@ -187,8 +203,7 @@ class App extends Component {
                       />
                       </div>
                       )
-                    }
-                
+                    }                
               </div>
             </section>
           </div>
@@ -197,5 +212,4 @@ class App extends Component {
     );
   }
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import StartApp from './StartApp';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+//Chiamo il metodo render. Aggancio il componente radice.
 ReactDOM.render(<StartApp />, document.getElementById('root'));
 
